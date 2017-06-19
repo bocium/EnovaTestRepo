7dc94fed64f52482b1c1f14fae0ef8153ef59774
bocium <bocium@outlook.com>
2017-06-19 19:36:53 +0200

    Calculation added

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int calculationResult = Calculate(4, 5);
+            Console.WriteLine("Result of the calculation: {0}", calculationResult);
+
             Console.ReadLine();
+        }
+
+        static int Calculate(int a, int b)
+        {
+            int result = a * b;
+
+            return result;
         }
     }
 }
